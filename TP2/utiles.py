782fdce1584c139c10f7e17861bed1a0e8e87392
Carmen Alvarez Hayes <carmenalvarezhayes@gmail.com>
2022-04-22 20:53:45 -0300

    odificaciones en utiles.py

--- TP2/utiles.py
+++ TP2/utiles.py
@@ -1,2 +1,24 @@
 def vocales (cadena):
     print(list(filter(lambda l: l.lower() in "aeiou",cadena)))
+
+
+import json
+
+archivo = open("bandas.txt", "w")
+datos = [
+{"nombre": "William Campbell", "ciudad": "La Plata", "ref": "www.instagram.com/williamcampbellok"},
+{"nombre": "Buendia", "ciudad": "La Plata", "ref":"https://buendia.bandcamp.com/"},
+{"nombre": "Lúmine", "ciudad": "La Plata", "ref": "https://www.instagram.com/luminelp/"}]
+print(type(datos))
+json.dump(datos, archivo)
+archivo.close()
+
+
+archivo = open("bandas.txt", "r")
+datos = json.load(archivo)
+#print(type(datos))
+#print(datos)
+datos_a_mostrar = json.dumps(datos, indent=4)
+print(datos_a_mostrar)
+print(type(datos_a_mostrar))
+archivo.close()
